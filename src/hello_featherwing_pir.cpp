daa8b872c23c2d9eb40e9dbd870a986cfe717b78
Peter Radcliffe (pir) <pir-github@pir.net>
2026-01-18 23:03:12 +0000

    Add a button to example to test touchscreen

--- src/hello_featherwing_pir.cpp
+++ src/hello_featherwing_pir.cpp
@@ -14,6 +14,8 @@
 #define TSC2007_TS 0 // Set this to 1 for V2 TSC2007 touchscreen displays
 #endif
 
+#define GLUE_LOCKING
+
 #include <Arduino.h>
 
 #include <Adafruit_LvGL_Glue.h> // Always include this BEFORE lvgl.h!
@@ -44,15 +46,57 @@
   #include <Adafruit_ILI9341.h>
   Adafruit_ILI9341 tft(TFT_CS, TFT_DC);
 #endif
-
 #if TSC2007_TS
 #define TSC_IRQ STMPE_CS
 Adafruit_TSC2007 ts=Adafruit_TSC2007();
 #else
 Adafruit_STMPE610  ts(STMPE_CS);
 #endif
-
 Adafruit_LvGL_Glue glue;
+
+static void btn_event_cb(lv_event_t * e);
+void lv_button(void);
+
+static void btn_event_cb(lv_event_t * e) {
+#ifdef GLUE_LOCKING
+  glue.lvgl_acquire();
+#endif //GLUE_LOCKING
+  lv_event_code_t code = lv_event_get_code(e);
+  lv_obj_t * btn = lv_event_get_target(e);
+  if(code == LV_EVENT_CLICKED) {
+    static uint8_t cnt = 0;
+    cnt++;
+    Serial.println("Button clicked");
+    /*Get the first child of the button which is the label and change its text*/
+    lv_obj_t * label = lv_obj_get_child(btn, 0);
+    lv_label_set_text_fmt(label, "Button: %d", cnt);
+  }
+#ifdef GLUE_LOCKING
+  glue.lvgl_release();
+#endif //GLUE_LOCKING
+}
+
+/**
+ * Create a button with a label and react on click event.
+ */
+void lv_button(void) {
+#ifdef GLUE_LOCKING
+  // Glue locking here locks things up, do not know why
+  glue.lvgl_acquire();
+#endif //GLUE_LOCKING
+  lv_obj_t * btn = lv_btn_create(lv_scr_act());     /*Add a button the current screen*/
+  lv_obj_set_pos(btn, 10, 10);                            /*Set its position*/
+  lv_obj_set_size(btn, 120, 50);                          /*Set its size*/
+  lv_obj_add_event_cb(btn, btn_event_cb, LV_EVENT_ALL, NULL);           /*Assign a callback to the button*/
+
+#ifdef GLUE_LOCKING
+  glue.lvgl_release();
+#endif //GLUE_LOCKING
+  // Doing this with the glue lock on hangs?
+  lv_obj_t * label = lv_label_create(btn);          /*Add a label to the button*/
+  lv_label_set_text(label, "Button");                     /*Set the labels text*/
+  lv_obj_center(label);
+}
 
 // This example sketch's LittlevGL UI-building calls are all in this
 // function rather than in setup(), so simple programs can just
@@ -60,9 +104,9 @@ Adafruit_LvGL_Glue glue;
 void lvgl_setup(void) {
   // Locks LVGL resource to prevent memory corrupton on ESP32
   // When using WiFi and LVGL_Glue, this function MUST be called PRIOR to LVGL function (`lv_`) calls
-#ifdef ESP32
+#ifdef GLUE_LOCKING
   glue.lvgl_acquire();
-#endif //ESP32
+#endif //GLUE_LOCKING
 
   // Create simple label centered on screen
   lv_obj_t *label = lv_label_create(lv_scr_act());
@@ -71,9 +115,9 @@ void lvgl_setup(void) {
 
   // Unlocks LVGL resource to prevent memory corrupton on ESP32
   // NOTE: This function MUST be called AFTER lvgl_acquire
-#ifdef ESP32
+#ifdef GLUE_LOCKING
   glue.lvgl_release();
-#endif //ESP32
+#endif //GLUE_LOCKING
 }
 
 void setup(void) {
@@ -107,6 +151,7 @@ void setup(void) {
   }
 
   lvgl_setup(); // Call UI-building function above
+  lv_button();  // Add a button
 }
 
 void loop(void) {
